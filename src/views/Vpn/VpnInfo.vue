0f40242fb26dd9cd7f2235fe6970301ad1deda8f
qq1788lover <a17868810687@gmail.com>
2021-09-28 23:20:11 +0000

    带来

--- src/views/Vpn/VpnInfo.vue
+++ src/views/Vpn/VpnInfo.vue
@@ -3,7 +3,7 @@
         <h2>账号信息</h2>
         <ul>
             <li>
-                <span>服务器地址：</span><input v-model="domain" type="text" name="服务器地址" disabled/>
+                <span>服务器地址：</span><input v-model="node" type="text" name="服务器地址" disabled/>
             </li>
             <li>
                 <span>服务器用户名：</span><input v-model="user" type="text" name="服务器用户名" disabled/>
@@ -20,21 +20,32 @@
             <li>
                 <span>剩余天数：</span><input v-model="time_remaining" type="text" name="剩余天数" disabled/>
             </li>
+            <li class="nodes">
+                <span>其他服务器：</span>
+                <ul>
+                    <li v-for="(tmp,index) in select_tmp" :key="index">
+                       <label>
+                           <span>{{tmp['proxy'] ? tmp['proxy'] : tmp['tmp_domain']}}</span>
+                           <input type="radio" :value="tmp['tmp_domain']" v-model="checked"/>
+                       </label>
+                    </li>
+                </ul>
+            </li>
         </ul>
-        <div class="radios">
-            <span>其他服务器：</span>
-            <div>
-                <label v-for="(tmp,index) in select_tmp" :key="index">
-                    <span>{{tmp['tmp_domain']}}</span>
-                    <input type="radio" :value="tmp['tmp_domain']" v-model="checked"/>
-                </label>
-            </div>
-        </div>
+<!--                <div class="radios">-->
+<!--                    <span>其他服务器：</span>-->
+<!--                    <div>-->
+<!--                        <label v-for="(tmp,index) in select_tmp" :key="index">-->
+<!--                            <span>{{tmp['proxy'] ? tmp['proxy'] : tmp['tmp_domain']}}</span>-->
+<!--                            <input type="radio" :value="tmp['tmp_domain']" v-model="checked"/>-->
+<!--                        </label>-->
+<!--                    </div>-->
+<!--                </div>-->
     </div>
 </template>
 
 <script>
-    import {ref, reactive, toRefs, onActivated, onMounted, watch} from "vue";
+    import {onMounted, reactive, ref, toRefs, watch} from "vue";
     import {get, patch} from "@/network";
 
     export default {
@@ -42,7 +53,9 @@
         setup() {
             let checked = ref(-1)
             const info = reactive({
-                domain: "",
+                // domain: "",
+                // proxy:"",
+                node: "",
                 user: '',
                 passwd: "",
                 vpn_passwd: '',
@@ -58,7 +71,9 @@
                 get("/vpn/item/").then(
                     res => {
                         console.log(res.data)
-                        info.domain = res.data['domain']
+                        // info.domain = res.data['domain']
+                        // info.proxy = res.data['proxy']
+                        info.node = res.data['proxy'] ? res.data['proxy'] : res.data['domain']
                         info.user = res.data['user']
                         info.vpn_passwd = res.data['vpn_passwd']
                         info.create_time = res.data['create_time']
@@ -83,7 +98,7 @@
                         res => {
                             alert("更换成功！请在软件里重新配置新的服务器地址")
                             // console.log(res.data);
-                            info.domain = res.data['domain']
+                            info.node = res.data['proxy'] ? res.data['proxy'] : res.data['domain']
                             info.create_time = res.data['create_time']
                             info.select_tmp = res.data['select_tmp']
                             checked.value = -1
@@ -111,7 +126,8 @@
     div {
         /*height:calc(100vh - 100px);*/
         height: inherit;
-        width:100%;
+        width: 100%;
+
         h2 {
             text-align: center;
         }
@@ -136,45 +152,75 @@
                     height: 40px;
                 }
             }
-        }
 
-        .radios {
-            width: 100%;
-            display: flex;
-            height: auto;
-            span {
-                display: block;
-                width: 35%;
-                text-align: center;
-            }
+            li:nth-last-child(1) {
+                /*background-color: red;*/
 
-            div {
-                margin-bottom: 10px;
-                flex: 1;
-                display: flex;
-                flex-direction: column;
-                /*padding-left: 10px;*/
-                width: 55%;
-                /*background-color: #F8F8F8;*/
-                /*border: 10px solid #CC0000;*/
-                label {
-                    background-color: #F8F8F8;
-                    display: block;
-                    padding-left: 10px;
-                    width: 80%;
-                    border: 1px solid rgba(118, 118, 118, 0.3);
+                span {
+                    display: inline-block;
+                }
 
-                    span {
+                ul {
+                    display: inline-block;
+                    width: 55%;
+                    li {
+                        border: 1px solid rgba(118, 118, 118, 0.3);
+                        /*border-color: #F8F8F8;*/
                         height: 40px;
-                        line-height: 40px;
-                        display: inline;
-                    }
-
-                    input {
-                        display: inline;
+                        background-color: #F8F8F8;
+                        span {
+                            display: inline-block;
+                        }
+                        input {
+                            margin: 12.5px 3px;
+                            display: inline-block;
+                            float: left;
+                            width: 15px;
+                            height: 15px;
+                        }
                     }
                 }
             }
         }
+
+        /*.radios {*/
+        /*    width: 100%;*/
+        /*    display: flex;*/
+        /*    height: auto;*/
+
+        /*    span {*/
+        /*        display: block;*/
+        /*        width: 35%;*/
+        /*        text-align: center;*/
+        /*    }*/
+
+        /*    div {*/
+        /*        margin-bottom: 10px;*/
+        /*        flex: 1;*/
+        /*        display: flex;*/
+        /*        flex-direction: column;*/
+        /*        !*padding-left: 10px;*!*/
+        /*        width: 55%;*/
+        /*        !*background-color: #F8F8F8;*!*/
+        /*        !*border: 10px solid #CC0000;*!*/
+        /*        label {*/
+        /*            background-color: #F8F8F8;*/
+        /*            display: block;*/
+        /*            padding-left: 10px;*/
+        /*            width: 90%;*/
+        /*            border: 1px solid rgba(118, 118, 118, 0.3);*/
+
+        /*            span {*/
+        /*                height: 40px;*/
+        /*                line-height: 40px;*/
+        /*                display: inline;*/
+        /*            }*/
+
+        /*            input {*/
+        /*                display: inline;*/
+        /*            }*/
+        /*        }*/
+        /*    }*/
+        /*}*/
     }
 </style>
